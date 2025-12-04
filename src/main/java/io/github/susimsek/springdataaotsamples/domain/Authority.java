7012af5b106a25e512f1326c4e4d86b1d7134ae4
Şuayb Şimşek <suayb.simsek@ykteknoloji.com.tr>
2025-12-04 19:18:35 +0300

    refactor: implement equals and hashCode methods in Authority class

--- src/main/java/io/github/susimsek/springdataaotsamples/domain/Authority.java
+++ src/main/java/io/github/susimsek/springdataaotsamples/domain/Authority.java
@@ -11,6 +11,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "authority")
@@ -27,4 +30,32 @@ public class Authority {
 
     @Column(nullable = false, unique = true, length = 50)
     private String name;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        Class<?> oEffectiveClass = o instanceof HibernateProxy hibernateProxy
+            ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass()
+            : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy hibernateProxy
+            ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass()
+            : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) {
+            return false;
+        }
+        Authority authority = (Authority) o;
+        return getId() != null && Objects.equals(getId(), authority.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy hibernateProxy
+            ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass().hashCode()
+            : getClass().hashCode();
+    }
 }
