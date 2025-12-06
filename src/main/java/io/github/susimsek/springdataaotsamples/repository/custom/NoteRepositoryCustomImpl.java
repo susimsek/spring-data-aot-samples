3c9f387849ea433ce30d254a3e8701ad1148102a
Şuayb Şimşek <suayb.simsek@ykteknoloji.com.tr>
2025-12-06 11:52:41 +0300

    refactor: remove nullable annotation from findIds method

--- src/main/java/io/github/susimsek/springdataaotsamples/repository/custom/NoteRepositoryCustomImpl.java
+++ src/main/java/io/github/susimsek/springdataaotsamples/repository/custom/NoteRepositoryCustomImpl.java
@@ -7,7 +7,6 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
-import org.jspecify.annotations.Nullable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
